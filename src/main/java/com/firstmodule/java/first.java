140b15193681236f75dfc3c6a6aa1de7d2dd378a
GurjarHemendra <connecthemendra29@gmail.com>
2022-09-14 12:22:22 +0530

    FORTH COMMIT GLOBAL -@ 12:22

--- src/main/java/com/firstmodule/java/first.java
+++ src/main/java/com/firstmodule/java/first.java
@@ -2,9 +2,14 @@ package com.firstmodule.java;
 
 public class first
 {
+    static
+    {
+        System.out.println("Static block from - commit global ");
+    }
     public static void main(String[] args) {
         System.out.println("welcome to java program , its my first file ");
         System.out.println("second commit");
         System.out.println("third commit @12:19");
+
     }
 }
