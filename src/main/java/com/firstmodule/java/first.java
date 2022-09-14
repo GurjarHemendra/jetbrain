fd8d8dcf740ea0f522477612c36bb7c15ae78e54
GurjarHemendra <connecthemendra29@gmail.com>
2022-09-14 12:15:32 +0530

    second commit

--- src/main/java/com/firstmodule/java/first.java
+++ src/main/java/com/firstmodule/java/first.java
@@ -4,5 +4,6 @@ public class first
 {
     public static void main(String[] args) {
         System.out.println("welcome to java program , its my first file ");
+        System.out.println("second commit");
     }
 }
