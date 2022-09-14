714ccf24dce17cf5475c3e29dfb242f77e3820c0
GurjarHemendra <connecthemendra29@gmail.com>
2022-09-14 12:13:33 +0530

    first commit (push code to github)

---
+++ src/main/java/com/firstmodule/java/first.java
@@ -0,0 +1,8 @@
+package com.firstmodule.java;
+
+public class first
+{
+    public static void main(String[] args) {
+        System.out.println("welcome to java program , its my first file ");
+    }
+}
